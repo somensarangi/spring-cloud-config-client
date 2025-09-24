e12bb059fa06a2749e728cd9a0042df0a4767bd4
somensarangi <somen.30oct@gmail.com>
2025-09-24 21:17:40 +0530

    Init Project

---
+++ src/main/java/com/example/spring_cloud_config_client/SpringCloudConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_config_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+
+}
